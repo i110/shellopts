23ed8338ec835c32500531642b7bdda2dfc055ea
Ichito Nagata <i.nagata110@gmail.com>
2024-08-07 15:05:58 +0900

    ignore unknown options

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,11 @@ use serde::{de::Visitor, Deserialize, Deserializer};
 use serde_json;
 use shell_quote::Bash;
 use std::env;
-use std::ffi::OsString;
 use std::fmt::Write;
 use std::fs::File;
 use std::io::BufReader;
 
-static POSITIONAL_ARGUMENTS_ID: &str = "positional_arguments";
+// static POSITIONAL_ARGUMENTS_ID: &str = "positional_arguments";
 
 #[derive(Debug, Clone)]
 enum Validator {
@@ -86,12 +85,14 @@ impl<'de> Deserialize<'de> for Validator {
 #[command(version, about, long_about = None)]
 struct AppOpts {
     #[arg(short, long)]
+    name: Option<String>,
+    #[arg(short, long)]
     schema: String,
     #[arg(long)]
     varname_prefix: Option<String>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Default)]
 struct SchemaOpt {
     name: String,
     short: Option<String>,
@@ -123,16 +124,41 @@ impl SchemaOpt {
         }
         return Some("VALUE");
     }
+
+    fn is_hit(&self, arg: &str) -> bool {
+        if let Some(short) = self.short.as_ref() {
+            if self.takes_value {
+                if arg.starts_with(&format!("-{}=", short)) {
+                    return true;
+                }
+            } else {
+                if arg == format!("-{}", short) {
+                    return true;
+                }
+            }
+        }
+        if self.takes_value {
+            if arg.starts_with(&format!("--{}=", self.name)) {
+                return true;
+            }
+        } else {
+            if arg == format!("--{}", self.name) {
+                return true;
+            }
+        }
+        false
+    }
 }
 
 #[derive(Debug, Deserialize)]
 struct Schema {
-    name: Option<String>,
+    #[serde(default)]
+    ignore_unknown_options: bool,
     options: Vec<SchemaOpt>,
 }
 
-fn build_command(schema: &Schema) -> Command {
-    let mut command = Command::new(schema.name.clone().unwrap_or("hoge".into()))
+fn build_command(schema: &Schema, app_name: &str) -> Command {
+    let mut command = Command::new(app_name.to_owned())
         .no_binary_name(true)
         .disable_version_flag(true);
     for option in schema.options.iter() {
@@ -162,13 +188,13 @@ fn build_command(schema: &Schema) -> Command {
 
         command = command.arg(arg);
     }
-    command = command.arg(Arg::new(POSITIONAL_ARGUMENTS_ID).num_args(0..).hide(true));
+    // command = command.arg(Arg::new(POSITIONAL_ARGUMENTS_ID).num_args(0..).hide(true));
     command
 }
 
 struct Parsed<'a> {
-    options: Vec<(&'a SchemaOpt, &'a str)>,
-    arguments: Vec<&'a str>,
+    options: Vec<(&'a SchemaOpt, String)>,
+    arguments: Vec<String>,
 }
 
 fn generate_script(parsed: Parsed, app_opts: AppOpts) -> anyhow::Result<String> {
@@ -181,7 +207,7 @@ fn generate_script(parsed: Parsed, app_opts: AppOpts) -> anyhow::Result<String> 
             opt.name.as_str().to_uppercase()
         };
 
-        let escaped = String::from_utf8(Bash::quote_vec(value))?;
+        let escaped = String::from_utf8(Bash::quote_vec(&value))?;
         writeln!(&mut buffer, "export {}={}", varname, escaped).unwrap();
     }
 
@@ -191,10 +217,10 @@ fn generate_script(parsed: Parsed, app_opts: AppOpts) -> anyhow::Result<String> 
     Ok(buffer)
 }
 
-fn parse<'a>(schema: &'a Schema, m: &'a ArgMatches) -> Parsed<'a> {
+fn generate_parsed<'a>(schema: &'a Schema, m: ArgMatches, pargs: Vec<&str>) -> Parsed<'a> {
     let mut parsed = Parsed {
         options: vec![],
-        arguments: vec![],
+        arguments: pargs.into_iter().map(|x| x.to_string()).collect(),
     };
 
     for id in m.ids() {
@@ -205,33 +231,66 @@ fn parse<'a>(schema: &'a Schema, m: &'a ArgMatches) -> Parsed<'a> {
         };
 
         let value = if let Some(value) = if opt.takes_value {
-            m.get_one::<String>(id.as_str()).map(|x| x.as_str())
+            Some(m.get_one::<String>(id.as_str()).map(|x| x.as_str()))
         } else {
             m.get_one::<bool>(id.as_str())
-                .map(|&x| if x { "1" } else { "0" })
+                .map(|&x| if x { Some("1") } else { None })
         } {
             value
         } else {
             continue;
         };
 
-        parsed.options.push((opt, value));
-    }
-
-    if let Some(args) = m.get_many::<String>(POSITIONAL_ARGUMENTS_ID) {
-        for arg in args {
-            parsed.arguments.push(arg);
+        if let Some(value) = value {
+            parsed.options.push((opt, value.to_owned()));
         }
     }
 
     parsed
 }
 
+fn split_into_options_and_positional_arguments<'a>(
+    schema: &Schema,
+    args: &[&'a str],
+) -> (Vec<&'a str>, Vec<&'a str>) {
+    let mut options = vec![];
+    let mut pargs = vec![];
+    for &arg in args {
+        let opt = schema.options.iter().find(|opt| opt.is_hit(arg));
+        if arg.starts_with("-") {
+            if schema.ignore_unknown_options && opt.is_none() {
+                pargs.push(arg);
+            } else {
+                options.push(arg);
+            }
+        } else {
+            pargs.push(arg);
+        }
+    }
+    (options, pargs)
+}
+
+fn parse<'a>(schema: &'a Schema, app_name: &str, args: &[&str]) -> anyhow::Result<Parsed<'a>> {
+    let (option_args, pargs) = split_into_options_and_positional_arguments(schema, args);
+
+    let command = build_command(schema, app_name);
+    let m = command.try_get_matches_from(option_args)?;
+    Ok(generate_parsed(&schema, m, pargs))
+}
+
 fn main() -> anyhow::Result<()> {
-    let args: Vec<OsString> = env::args_os().collect();
+    let args: Vec<String> = env::args_os()
+        .map(|x| x.into_string())
+        .collect::<Result<Vec<_>, _>>()
+        .map_err(|_| anyhow::Error::msg("invalid utf-8 sequence"))?;
     let mut splitted = args.split(|arg| arg == "--");
     let app_args = splitted.next().unwrap_or_default();
-    let target_args = splitted.next().unwrap_or_default();
+    let target_args = splitted
+        .next()
+        .unwrap_or_default()
+        .into_iter()
+        .map(|x| x.as_str())
+        .collect::<Vec<_>>();
 
     let app_opts = AppOpts::parse_from(app_args);
 
@@ -243,12 +302,89 @@ fn main() -> anyhow::Result<()> {
         serde_json::from_str(&app_opts.schema)?
     };
 
-    let command = build_command(&schema);
-    let m = command.try_get_matches_from(target_args)?;
+    let parsed = parse(
+        &schema,
+        app_opts.name.as_deref().unwrap_or("PROGRAM"),
+        &target_args,
+    )?;
 
-    let parsed = parse(&schema, &m);
     let script = generate_script(parsed, app_opts)?;
     println!("{}", script);
 
     anyhow::Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        let mut schema = Schema {
+            ignore_unknown_options: true,
+            options: vec![
+                SchemaOpt {
+                    name: "foo".into(),
+                    takes_value: true,
+                    required: true,
+                    ..Default::default()
+                },
+                SchemaOpt {
+                    name: "bar".into(),
+                    ..Default::default()
+                },
+            ],
+        };
+        let parsed = parse(&schema, "", &["--foo=XXX".into(), "--bar".into()]).unwrap();
+        assert_eq!(parsed.options.len(), 2);
+        assert_eq!(parsed.options[0].1, "XXX");
+        assert_eq!(parsed.options[1].1, "1");
+
+        let parsed = parse(&schema, "", &["--foo=XXX".into()]).unwrap();
+        assert_eq!(
+            parsed.options.len(),
+            1,
+            "no required flag doesn't appear in parsed"
+        );
+
+        let parsed = parse(
+            &schema,
+            "",
+            &[
+                "aaa".into(),
+                "--unknown1".into(),
+                "--foo=XXX".into(),
+                "-u".into(),
+                "--bar".into(),
+                "bbb".into(),
+                "--unknown3".into(),
+            ],
+        )
+        .unwrap();
+        assert_eq!(parsed.options.len(), 2);
+        assert_eq!(parsed.options[0].1, "XXX");
+        assert_eq!(parsed.options[1].1, "1");
+        assert_eq!(parsed.arguments.len(), 5);
+
+        schema.ignore_unknown_options = false;
+        let parsed = parse(
+            &schema,
+            "",
+            &[
+                "aaa".into(),
+                "--unknown1".into(),
+                "--foo=XXX".into(),
+                "-u".into(),
+                "--bar".into(),
+                "bbb".into(),
+                "--unknown3".into(),
+            ],
+        );
+        assert!(parsed.is_err());
+        assert!(parsed
+            .err()
+            .unwrap()
+            .to_string()
+            .starts_with("error: unexpected argument '--unknown1' found"));
+    }
 }
